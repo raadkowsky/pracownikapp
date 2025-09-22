ac211d1ba80faa559a8ebb665d3c785e55656401
Majewski Radosław ECN <radoslaw.majewski@enea.pl>
2025-09-22 02:22:25 +0200

    poprawa pliku pwa-installer

--- dashboard/js/pwa-installer.js
+++ dashboard/js/pwa-installer.js
@@ -6,7 +6,7 @@
 if ('serviceWorker' in navigator) {
    window.addEventListener('load', () => {
       navigator.serviceWorker
-         .register('/service-worker.js')
+         .register('service-worker.js')
          .then((registration) => {
             console.log('Service Worker zarejestrowany pomyślnie!');
          })
